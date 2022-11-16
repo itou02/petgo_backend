ff7c6553089472b40415d17f747fa80772fbec7f
a23405 <a2340500789@gmail.com>
2022-11-16 19:01:22 +0800

    fix: :ambulance: 大改

--- app/Http/Service/UserService.php
+++ app/Http/Service/UserService.php
@@ -19,11 +19,6 @@ class UserService
     {
         $this->pet = new PetService();
     }
-    // public function getAllUser()
-    // {
-    //     $users = User::orderBy('created_at')->get();
-    //     return $users;
-    // }
 
     // 會員資料 - 個人
     public function UserInfo($request)
@@ -155,19 +150,19 @@ class UserService
         ]);
     }
 
-    // 申請 - 基本資料
-    public function ApplyBasicInfo()
-    {
-        $id = Auth::user()->id;
-        $info = DB::table('users')
-            ->join('locations', 'users.location_id', '=', 'locations.id')
-            ->select(
-                'users.name',
-                'users.sex',
-                'locations.location',
-            )
-            ->where('users.id', '=', $id)
-            ->get();
-        return $info;
-    }
+    // // 申請 - 基本資料
+    // public function ApplyBasicInfo()
+    // {
+    //     $id = Auth::user()->id;
+    //     $info = DB::table('users')
+    //         ->join('locations', 'users.location_id', '=', 'locations.id')
+    //         ->select(
+    //             'users.name',
+    //             'users.sex',
+    //             'locations.location',
+    //         )
+    //         ->where('users.id', '=', $id)
+    //         ->get();
+    //     return $info;
+    // }
 }
